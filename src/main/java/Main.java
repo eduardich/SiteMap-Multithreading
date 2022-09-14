52ab3974da940e3f24e59b985594307da20a97c0
Panik Daniil <dpanikxxi@gmail.com>
2022-09-14 12:33:04 +0300

    readme edited, main.java comment deleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,7 +23,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        long start = System.currentTimeMillis();
         try {
 
             createDirectoryAndFile(SITE_MAP_PATH);
@@ -35,8 +34,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        System.out.println(System.currentTimeMillis() - start);
 
     }
 
